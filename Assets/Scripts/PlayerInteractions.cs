97308ff91768086c72091ab30df1294e44e8a4f8
Markus Rychter <89453088+Marryc02@users.noreply.github.com>
2023-02-03 23:20:14 +0100

    Tested with bababooey script

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
+    // [SerializeField] float bababooey = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
