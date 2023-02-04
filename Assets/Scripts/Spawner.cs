8b43c6c59fcbd1fb1a8a3c472f4fb0a27cbba3b4
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-02-04 20:48:08 +0100

    more spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,10 +7,42 @@ public class Spawner : MonoBehaviour
 
     [SerializeField] GameObject EnemyToSpawn;
     [SerializeField] GameObject SpawnLocation;
+
+    [SerializeField] Transform TreePoint;
     // Start is called before the first frame update
+    
+    int num = 500; // Amount of enemies to spawn
+    Vector3 point; // The point we spawn around
+    float radius = 50; // How far away they spawn
+    
     void Start()
     {
-        Instantiate(EnemyToSpawn, SpawnLocation.transform.position, transform.rotation);
+        point = TreePoint.position;
+        for (int i = 0; i < num; i++)
+        {
+ 
+            /* Distance around the circle */
+            var radians = 2 * Mathf.PI / num * i;
+
+            /* Get the vector direction */
+            var vertical = Mathf.Sin(radians);
+            var horizontal = Mathf.Cos(radians);
+    
+            var spawnDir = new Vector3(horizontal, 0, vertical);
+    
+            /* Get the spawn position */
+            var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
+    
+            /* Now spawn */
+            var enemy = Instantiate(EnemyToSpawn, spawnPos, Quaternion.identity) as GameObject;
+    
+            /* Rotate the enemy to face towards player */
+            enemy.transform.LookAt(point);
+    
+            /* Adjust height */
+            enemy.transform.Translate(new Vector3(0, enemy.transform.localScale.y / 2, 0));
+        }
+        
     }
 
     // Update is called once per frame
