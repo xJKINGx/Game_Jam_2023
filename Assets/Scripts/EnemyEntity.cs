2781373c7520731d946f81646309ebcf08de32d0
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-02-04 13:52:13 +0100

    Scripts for enemies and friendlies
    gongaga

---
+++ Assets/Scripts/EnemyEntity.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEntity : MonoBehaviour
+{
+    private bool bIsAlive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
