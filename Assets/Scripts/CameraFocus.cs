605e7ccda606c55b45771fe7f88ab9bd66c9d000
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-02-04 15:01:40 +0100

    Tree object and camera movement

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+
+    [SerializeField] float Sensitivity = 0.4f;
+    [SerializeField] Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Debug.Log("bababooey");
+            transform.localEulerAngles += new Vector3(0.0f, Sensitivity, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localEulerAngles += new Vector3(0.0f, -Sensitivity, 0.0f);
+            //Debug.Log("Gongaga");
+        }
+
+        transform.position = Target.position - transform.forward * 10.0f;
+    }
+}
