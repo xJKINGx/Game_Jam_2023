0fb785cfc3e715ca66e98ebd3786646f70d1f7b5
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2023-02-05 01:46:26 +0100

    random spawners

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,33 +18,37 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         point = TreePoint.position;
-        for (int i = 0; i < num; i++)
-        {
- 
-            /* Distance around the circle */
-            var radians = 2 * Mathf.PI / num * i;
+        /*
+        float x = seconds before the first invoke;
+        float y = seconds between every invoke;
 
-            /* Get the vector direction */
-            var vertical = Mathf.Sin(radians);
-            var horizontal = Mathf.Cos(radians);
-    
-            var spawnDir = new Vector3(horizontal, 0, vertical);
-    
-            /* Get the spawn position */
-            var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
-    
-            /* Now spawn */
-            var enemy = Instantiate(EnemyToSpawn, spawnPos, Quaternion.identity) as GameObject;
-    
-            /* Rotate the enemy to face towards player */
-            enemy.transform.LookAt(point);
-    
-            /* Adjust height */
-            enemy.transform.Translate(new Vector3(0, enemy.transform.localScale.y / 2, 0));
-        }
-        
+        InvokeRepeating("Function", x, y);
+        */
+        InvokeRepeating("SpawnBois", 0f, 2.5f);
     }
 
+    void SpawnBois()
+    {
+        /* Distance around the circle */
+        var radians = 2 * Mathf.PI / num * Random.value * 100;
+        /* Get the vector direction */
+        var vertical = Mathf.Sin(radians);
+        var horizontal = Mathf.Cos(radians);
+   
+        var spawnDir = new Vector3(horizontal, 0, vertical);
+  
+        /* Get the spawn position */
+        var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
+   
+        /* Now spawn */
+        var enemy = Instantiate(EnemyToSpawn, spawnPos, Quaternion.identity) as GameObject;
+   
+        /* Rotate the enemy to face towards player */
+        enemy.transform.LookAt(point);
+   
+        /* Adjust height */
+        enemy.transform.Translate(new Vector3(0, enemy.transform.localScale.y / 2, 0));
+    }
     // Update is called once per frame
     void Update()
     {
